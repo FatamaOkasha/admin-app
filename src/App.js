263243bb7ead1129f32f7f99c67195cf89ef34f1
FatamaOkasha <fatmaokasha01@gmail.com>
2024-08-28 14:17:06 +0300

    Fix some bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import { FaSearch } from "react-icons/fa";
 import { IoMdTrash } from "react-icons/io";
@@ -9,11 +9,7 @@ function App() {
   const [filterRecords, setFilterRecords] = useState([]);
   const [search, setSearch] = useState("");
   const [editState, setEditState] = useState(-1);
-
-  //CheckBox
-  const [selectedRows, setSelectedRows] = useState([]);
-
-  //Pagination
+  const searchInputRef = useRef(null);
   const [currentPage, setCurrentPage] = useState(1);
   const recordsPerPage = 10;
   const lastIndex = currentPage * recordsPerPage;
@@ -22,75 +18,94 @@ function App() {
   const nPage = Math.ceil(records.length / recordsPerPage);
   const numbers = [...Array(nPage + 1).keys()].slice(1);
 
-  function handleSearch(event) {
-    let newData = filterRecords.filter((row) => {
-      // console.log("searchhh", search);
-      if (
-        row.name.toLowerCase().includes(search.toLowerCase()) ||
-        row.email.toLowerCase().includes(search.toLowerCase()) ||
-        row.role.toLowerCase().includes(search.toLowerCase()) ||
-        row.id.toLowerCase().includes(search.toLowerCase())
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-
-    newData.length = 3;
-    setRecords(newData);
-  }
-  const handleReset = () => {
-    console.log("reset was calikced");
-    const fetData = async () => {
-      axios
-        .get(
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
           "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
-        )
-        .then((res) => {
-          // console.log(res.data);
-          setRecords(res.data);
-          setFilterRecords(res.data);
-        })
-        .catch((error) => console.log(error));
+        );
+        setRecords(res.data);
+        setFilterRecords(res.data);
+      } catch (error) {
+        console.log(error);
+      }
     };
-    fetData();
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (searchInputRef.current) {
+      searchInputRef.current.focus();
+    }
+  }, [editState]);
+
+  function handleSearch() {
+    if (search === "") {
+      setRecords(filterRecords);
+    } else {
+      const newData = filterRecords
+        .filter((row) => {
+          return (
+            row.name.toLowerCase().includes(search.toLowerCase()) ||
+            row.email.toLowerCase().includes(search.toLowerCase()) ||
+            row.role.toLowerCase().includes(search.toLowerCase()) ||
+            row.id.toLowerCase().includes(search.toLowerCase())
+          );
+        })
+        .slice(0, 4); // Limit to 4 results
+
+      setRecords(newData);
+    }
+  }
+
+  const handleReset = () => {
+    setRecords(filterRecords);
+    setSearch("");
   };
+
   const handleCheckboxChange = (e) => {
     const { name, checked } = e.target;
     if (name === "allselect") {
-      const checkedvalue = records.slice(11, 10).map((row) => {
-        return { ...row, isChecked: checked };
-      });
-      setRecords(checkedvalue);
+      const updatedRecords = rows.map((row) => ({
+        ...row,
+        isChecked: checked,
+      }));
+      const allRecordsUpdated = records.map(
+        (record) => updatedRecords.find((row) => row.id === record.id) || record
+      );
+      setRecords(allRecordsUpdated);
     } else {
-      const checkedvalue = records.map((row) =>
+      const updatedRecords = records.map((row) =>
         row.name === name ? { ...row, isChecked: checked } : row
       );
-      setRecords(checkedvalue);
+      setRecords(updatedRecords);
     }
   };
+
   const handleAllDelete = () => {
     const recordsAfterDelete = records.filter((row) => !row.isChecked);
     setRecords(recordsAfterDelete);
-    console.log("was clicked");
   };
 
-  useEffect(() => {
-    const fetData = async () => {
-      axios
-        .get(
-          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
-        )
-        .then((res) => {
-          // console.log(res.data);
-          setRecords(res.data);
-          setFilterRecords(res.data);
-        })
-        .catch((error) => console.log(error));
-    };
-    fetData();
-  }, []);
+  const handleUpdateSubmit = (e) => {
+    e.preventDefault();
+    setEditState(-1);
+  };
+
+  const handleEditUser = (id) => {
+    setEditState(id);
+  };
+
+  const handleDelete = (id) => {
+    const updatedRecords = records.filter((row) => id !== row.id);
+    setRecords(updatedRecords);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
 
   return (
     <div className="App">
@@ -101,38 +116,48 @@ function App() {
           placeholder="Search..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={handleKeyPress} // Add keydown event handler
           style={{ padding: "6px 10px" }}
+          ref={searchInputRef}
         />
-        <FaSearch className="search-icon" onClick={() => handleSearch()} />
+        <FaSearch className="search-icon" onClick={handleSearch} />
         <input type="reset" className="reset-btn" onClick={handleReset} />
       </div>
-      <button class="delete-all-btn" onClick={handleAllDelete}>
+      <button className="delete-all-btn" onClick={handleAllDelete}>
         <IoMdTrash style={{ justifyContent: "center", fontSize: "15px" }} />
       </button>
       <form onSubmit={handleUpdateSubmit}>
         <table>
           <thead>
-            <th>
-              <input
-                type="checkbox"
-                name="allselect"
-                checked={!records.some((row) => row?.isChecked !== true)}
-                onChange={handleCheckboxChange}
-              />
-            </th>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Action</th>
+            <tr>
+              <th>
+                <input
+                  type="checkbox"
+                  name="allselect"
+                  checked={!records.some((row) => row?.isChecked !== true)}
+                  onChange={handleCheckboxChange}
+                />
+              </th>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
           </thead>
           <tbody>
-            {rows.map((row, i) =>
+            {rows.map((row) =>
               editState === row.id ? (
-                <EditUser row={row} records={records} setRecords={setRecords} />
+                <EditUser
+                  key={row.id}
+                  row={row}
+                  records={records}
+                  setRecords={setRecords}
+                  setEditState={setEditState} // Pass setEditState as a prop
+                />
               ) : (
                 <tr
-                  key={i}
+                  key={row.id}
                   style={{
                     backgroundColor: row.isChecked
                       ? "lightgray"
@@ -140,14 +165,12 @@ function App() {
                   }}
                 >
                   <td>
-                    {
-                      <input
-                        type="checkbox"
-                        name={row.name}
-                        checked={row?.isChecked || false}
-                        onChange={handleCheckboxChange}
-                      />
-                    }
+                    <input
+                      type="checkbox"
+                      name={row.name}
+                      checked={row?.isChecked || false}
+                      onChange={handleCheckboxChange}
+                    />
                   </td>
                   <td>{row.id}</td>
                   <td>{row.name}</td>
@@ -178,125 +201,163 @@ function App() {
       <nav>
         <ul className="pagination">
           <li className="page-item">
-            <a href="#" className="page-link" onClick={prePage}>
-              Prev
-            </a>
-          </li>
-          {numbers.map((n, i) => (
-            <li
-              className={`page-item ${currentPage === n ? "active" : ""}`}
-              key={i}
+            <button
+              className="page-link"
+              onClick={prePage}
+              disabled={currentPage === 1}
             >
-              <a
-                href="#"
+              Prev
+            </button>
+          </li>
+          {numbers.map((n) => (
+            <li
+              key={n}
+              className={`page-item ${currentPage === n ? "active" : ""}`}
+            >
+              <button
                 className="page-link"
                 onClick={() => changeCurrentPage(n)}
               >
                 {n}
-              </a>
+              </button>
             </li>
           ))}
           <li className="page-item">
-            <a href="#" className="page-link" onClick={nextPage}>
+            <button
+              className="page-link"
+              onClick={nextPage}
+              disabled={currentPage === nPage}
+            >
               Next
-            </a>
+            </button>
           </li>
         </ul>
       </nav>
     </div>
   );
-  //Pgination Functions
+
   function prePage() {
     if (currentPage !== 1) {
       setCurrentPage(currentPage - 1);
     }
   }
+
   function changeCurrentPage(id) {
     setCurrentPage(id);
   }
+
   function nextPage() {
     if (currentPage !== nPage) {
       setCurrentPage(currentPage + 1);
     }
   }
-  // Handle Edit & Delete functions
-  function handleUpdateSubmit(e) {
-    e.preventDefault();
-    setEditState(-1);
+}
+
+function EditUser({ row, records, setRecords, setEditState }) {
+  const [editField, setEditField] = useState(null);
+  const [tempRecord, setTempRecord] = useState(row);
+
+  const nameInputRef = useRef(null);
+  const emailInputRef = useRef(null);
+  const roleInputRef = useRef(null);
+
+  useEffect(() => {
+    switch (editField) {
+      case "name":
+        if (nameInputRef.current) {
+          nameInputRef.current.focus();
+        }
+        break;
+      case "email":
+        if (emailInputRef.current) {
+          emailInputRef.current.focus();
+        }
+        break;
+      case "role":
+        if (roleInputRef.current) {
+          roleInputRef.current.focus();
+        }
+        break;
+      default:
+        break;
+    }
+  }, [editField]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setTempRecord({ ...tempRecord, [name]: value });
   }
-  function handleEditUser(id) {
-    setEditState(id);
-  }
-  function handleDelete(id) {
-    const updatedRecords = records.filter((row) => id !== row.id);
+
+  function handleSave() {
+    const updatedRecords = records.map((r) =>
+      r.id === tempRecord.id ? { ...r, ...tempRecord } : r
+    );
     setRecords(updatedRecords);
+    setEditState(-1); // Reset editState to exit edit mode
   }
-  function EditUser({ row, records, setRecords }) {
-    function handleName(event) {
-      const name = event.target.value;
-      const updatedRecords = records.map((r) =>
-        r.id === row.id ? { ...r, name: name } : r
-      );
-      setRecords(updatedRecords);
-    }
-    function handleEmail(event) {
-      const email = event.target.value;
-      const updatedRecords = records.map((r) =>
-        r.id === row.id ? { ...r, email: email } : r
-      );
-      setRecords(updatedRecords);
-    }
-    function handleRole(event) {
-      const role = event.target.value;
-      const updatedRecords = records.map((r) =>
-        r.id === row.id ? { ...r, role: role } : r
-      );
-      setRecords(updatedRecords);
-    }
-    return (
-      <tr>
-        <td>
+
+  return (
+    <tr>
+      <td>
+        <input
+          type="text"
+          value={row.id}
+          name="id"
+          placeholder="Enter id"
+          disabled
+        />
+      </td>
+      <td>
+        {editField === "name" ? (
           <input
             type="text"
-            value={row.id}
-            name="id"
-            placeholder="Enter id"
-            disabled
-          />
-        </td>
-        <td>
-          <input
-            type="text"
-            value={row.name}
             name="name"
+            value={tempRecord.name}
             placeholder="Enter Name"
-            onChange={handleName}
+            onChange={handleChange}
+            ref={nameInputRef}
           />
-        </td>
-        <td>
+        ) : (
+          <span onClick={() => setEditField("name")}>{row.name}</span>
+        )}
+      </td>
+      <td>
+        {editField === "email" ? (
           <input
             type="email"
-            value={row.email}
             name="email"
-            placeholder="Enter Email"
-            onChange={handleEmail}
+            value={tempRecord.email}
+            placeholder="Enter email"
+            onChange={handleChange}
+            ref={emailInputRef}
           />
-        </td>
-        <td>
+        ) : (
+          <span onClick={() => setEditField("email")}>{row.email}</span>
+        )}
+      </td>
+      <td>
+        {editField === "role" ? (
           <input
             type="text"
-            value={row.role}
             name="role"
-            placeholder="Enter Role"
-            onChange={handleRole}
+            value={tempRecord.role}
+            placeholder="Enter role"
+            onChange={handleChange}
+            ref={roleInputRef}
           />
-        </td>
-        <td>
-          <button type="submit">Update</button>
-        </td>
-      </tr>
-    );
-  }
+        ) : (
+          <span onClick={() => setEditField("role")}>{row.role}</span>
+        )}
+      </td>
+      <td>
+        {editField ? (
+          <button className="update-btn" type="button" onClick={handleSave}>
+            Save
+          </button>
+        ) : null}
+      </td>
+    </tr>
+  );
 }
 
 export default App;
